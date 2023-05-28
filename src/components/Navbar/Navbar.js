a439f27ba3344d6ea0d6ae1fa70ea897e68a832f
hijoblimalika <nematxanovanaima@gmail.com>
2023-05-28 11:11:19 +0500

    dwddwdw

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import "./Navbar.css"
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <div>
+     <meta name='viewport' content='width=device-width, initial-scale=1' />
+  <section class="top-nav">
+    <div>
+      <li className="lol">32-maktab</li>
+    </div>
+    <input id="menu-toggle" type="checkbox" />
+    <label class='menu-button-container' for="menu-toggle">
+    <div class='menu-button'></div>
+  </label>
+    <ul class="menu">
+      <li><p>Our products</p></li>
+      <li><p>About Use</p></li>
+      <li><p>Contacts</p></li>
+      <li><p>Login</p></li>
+     
+    </ul>
+  </section>
+
+    </div>
     )
 }
 
