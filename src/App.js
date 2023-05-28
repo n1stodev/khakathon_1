6d5e9e3cea022c73f3e0cf3d7ed5e4ae05e38414
n1stodev <nurillotojiakhmedov@gmail.com>
2023-05-28 09:59:46 +0500

    dasdasd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Home from './router/home/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="" element="" />
+        <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
     </div>
