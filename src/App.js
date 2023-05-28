fda00bf89f8372fc15bf29e5894686731becaf7e
n1stodev <nurillotojiakhmedov@gmail.com>
2023-05-28 09:58:25 +0500

    dadas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="" element="" />
       </Routes>
+      <Footer />
     </div>
   );
 }
