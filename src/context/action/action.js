a4351c7cf8d3d67c79d7ac9744c18ff8cfc37a2b
n1stodev <nurillotojiakhmedov@gmail.com>
2023-05-28 11:10:40 +0500

    dasdasd

---
+++ src/context/action/action.js
@@ -0,0 +1,5 @@
+import { CLOSE_LOGIN, OPEN_LOGIN } from './actionType'
+
+export const closeLogin () => {
+    return { type: CLOSE_LOGIN }
+}
---
+++ src/context/reducer/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    water() { return 'Redux water' },
+    login
+})
+
+export default rootReducer
